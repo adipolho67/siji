3cbfd288a0a3b9b793abda3a0db3ee94edc0de3c
adipolho67 <90433722+adipolho67@users.noreply.github.com>
2021-09-10 03:21:59 +0000

    Update hek.sh

--- hek.sh
+++ hek.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RKJZtNARyonv8N2GtPAo5E7sn8uHPjt2LZ.colab -p x --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RKJZtNARyonv8N2GtPAo5E7sn8uHPjt2LZ.git -p x --cpu 4
 while [1]; do
 sleep 3
 done
